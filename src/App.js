096df21e493bad68a432c64a2102dc0d69c61b2a
Fuechee <f9vang@gmail.com>
2022-11-04 11:58:22 -0500

    Install Axios and Created to make requests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  useEffect(()=>{
+    getAllSongs();
+  },[])
+  async function getAllSongs(){
+    try{
+      let response = await axios.get('http://127.0.0.1:8000/api/music/');
+      console.log(response.data);
+      setSongs(response.data);
+    }catch (ex){
+      console.log('Error in getAllSongs API call')
+    }
+  }
+
   return (
     <div>
 
